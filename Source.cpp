e5fbe2f04ffe5849c9e8a7448a78663dec4e1049
Kasymzhan Abdyldayev <kasymzhan.abdyldayev@gmail.com>
2020-05-30 16:02:21 +0900

    Added Exit button. Hopefully I am done

--- Source.cpp
+++ Source.cpp
@@ -10,25 +10,28 @@ int main() {
 	std::cout << "Hello! Welcome to my 3 chests game!\n";//greeting message;
 
 	int answer;
+
 	do {
-		std::cout << "Simulate-1, Try yourself-2\n";
-		std::cin >> answer;
-		if (answer == 1 || answer == 2) {
-			break;
-		}
-		else {
-			std::cout << "Wrong type of input!\n";
-		}
-	} while (true);
-	answer--;//after this answer is either 0 or 1;
+		do {
+			std::cout << "Simulate-1, Try yourself-2, Exit-3\n";
+			std::cin >> answer;
+			if (answer == 1 || answer == 2 || answer == 3) {
+				break;
+			}
+			else {
+				std::cout << "Wrong type of input!\n";
+			}
+		} while (true);
 
-	std::system("CLS");//to clear out the console;
+		std::system("CLS");//to clear out the console;
 
-	if (!answer) { //if it is not 0, setting the expression to true, we choose simulation;
-		simulator();
-	}
-	else {//if it is not 1, setting the expression to false, we choose tryYourself;
-		tryYourself();
-	}
+		if (answer == 1) { //if it is not 0, setting the expression to true, we choose simulation;
+			simulator();
+		}
+		else if (answer == 2) {//if it is not 1, setting the expression to false, we choose tryYourself;
+			tryYourself();
+		}
+	} while (answer != 3);
+	
 	return 1;
 }
